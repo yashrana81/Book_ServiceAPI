82344a7c2369250f5e26b5605d8c9e2d00b074ca
yashrana81 <yashranamzn7788@gmail.com>
2023-08-10 02:19:29 +0530

    Addded file upload functionality.

---
+++ src/main/java/com/bookapi/restapi/contollers/FileUploadController.java
@@ -0,0 +1,48 @@
+package com.bookapi.restapi.contollers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.bookapi.restapi.helpers.FileUploadHelper;
+
+@RestController
+public class FileUploadController {
+
+    @Autowired
+    private FileUploadHelper fileUploadHelper;
+
+    @PostMapping("/uploadFile")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file){
+
+        // System.out.println(file.getOriginalFilename());
+        // System.out.println(file.getSize());
+        // System.out.println(file.getContentType());
+        // System.out.println(file.getName());
+
+        try {
+            if(file.isEmpty())
+        {
+            return ResponseEntity.internalServerError().body("File can't be empty");
+        }
+
+        if(!file.getContentType().equals("image/png") || file.getContentType().equals("image/jpeg"))
+        {
+            return ResponseEntity.internalServerError().body("file should be of image type");
+        }
+
+        boolean uploadStatus = fileUploadHelper.uploadFileHelper(file);
+        if(uploadStatus) return ResponseEntity.ok("file uploaded successfully");
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        
+        return ResponseEntity.internalServerError().body("something went wrong");
+        
+    }
+}
---
+++ src/main/java/com/bookapi/restapi/helpers/FileUploadHelper.java
@@ -0,0 +1,44 @@
+package com.bookapi.restapi.helpers;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileUploadHelper {
+
+    public final String uploadDir = "C:\\Java Programs\\restapi\\src\\main\\resources\\static\\image";
+    
+    public boolean uploadFileHelper(MultipartFile file)
+    {
+        boolean isSuccessful = false;
+        try {            
+            InputStream inputStream = file.getInputStream();
+            String filename = uploadDir+File.separator+file.getOriginalFilename();
+
+            // byte data[] = new byte[inputStream.available()];
+            // inputStream.read(data);
+            
+            // String filename = uploadDir+File.separator+file.getOriginalFilename();
+            // FileOutputStream fileOutputStream = new FileOutputStream(filename);
+            // fileOutputStream.flush();
+            // fileOutputStream.close();
+
+
+            //Better Method to upload
+            Files.copy(inputStream, Paths.get(filename), StandardCopyOption.REPLACE_EXISTING);
+            isSuccessful = true;
+        }catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return isSuccessful;
+    } 
+    
+}
